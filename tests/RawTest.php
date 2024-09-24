a61418922a185c939835f7dc4ca1f215ee78c0ed
Ewilan Rivière <ewilan.riviere@gmail.com>
2024-09-24 14:36:06 +0200

    Refactor assertion in RawTest.php to check if the response status code is true

--- tests/RawTest.php
+++ tests/RawTest.php
@@ -9,7 +9,7 @@ it('can send raw request', function () {
 
     expect($response)->not->toBeNull();
     expect($response->isSuccess())->toBeTrue();
-    expect($response->getStatusCode())->toBeArray();
+    expect($response->getStatusCode())->toBeTrue();
     expect($response->getUrl())->toBeString();
     expect($response->getBody())->toBeArray();
 });
