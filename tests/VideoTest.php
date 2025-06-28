b9ba80172b31b9a305dcf8c79aa78c47333a5519
Ewilan Rivière <ewilan.riviere@gmail.com>
2025-06-28 15:05:58 +0200

    add more tests

--- tests/VideoTest.php
+++ tests/VideoTest.php
@@ -3,53 +3,88 @@
 use Kiwilan\Tmdb\Models\Common\TmdbVideo;
 use Kiwilan\Tmdb\Tmdb;
 
-function apiCall()
+function apiCallMovie()
 {
     return Tmdb::client(apiKey())
         ->movies()
         ->details(movie_id: 696506, append_to_response: ['videos']);
 }
 
-it('can get teaser', function () {
-    $movie = apiCall();
+function apiCallTvShow()
+{
+    return Tmdb::client(apiKey())
+        ->tvSeries()
+        ->details(series_id: 87108, append_to_response: ['videos']);
+}
+
+function videoIs(TmdbVideo $video, string $type)
+{
+    expect($video)->toBeInstanceOf(TmdbVideo::class);
+    expect($video->getType())->toBe($type);
+    expect($video)->not()->toBeNull();
+    expect($video->getYouTubeUrl())->toBeString();
+}
+
+it('can get movie teaser', function () {
+    $movie = apiCallMovie();
     expect($movie->getVideos())->toBeArray();
 
-    $teaser = $movie->getVideoTeaser();
-    expect($teaser)->toBeInstanceOf(TmdbVideo::class);
-    expect($teaser->getType())->toBe('Teaser');
-    expect($teaser)->not()->toBeNull();
-    expect($teaser->getYouTubeUrl())->toBeString();
+    $video = $movie->getVideoTeaser();
+    videoIs($video, 'Teaser');
 });
 
-it('can get trailer', function () {
-    $movie = apiCall();
+it('can get movie trailer', function () {
+    $movie = apiCallMovie();
     expect($movie->getVideos())->toBeArray();
 
     $trailer = $movie->getVideoTrailer();
-    expect($trailer)->toBeInstanceOf(TmdbVideo::class);
-    expect($trailer->getType())->toBe('Trailer');
-    expect($trailer)->not()->toBeNull();
-    expect($trailer->getYouTubeUrl())->toBeString();
+    videoIs($trailer, 'Trailer');
 });
 
-it('can get promo', function () {
-    $movie = apiCall();
+it('can get movie promo', function () {
+    $movie = apiCallMovie();
     expect($movie->getVideos())->toBeArray();
 
     $trailer = $movie->getVideoPromo();
-    expect($trailer)->toBeInstanceOf(TmdbVideo::class);
-    expect($trailer->getType())->toBe('Trailer');
-    expect($trailer)->not()->toBeNull();
-    expect($trailer->getYouTubeUrl())->toBeString();
+    videoIs($trailer, 'Trailer');
 });
 
-it('can get trailer type', function () {
-    $movie = apiCall();
+it('can get movie trailer type', function () {
+    $movie = apiCallMovie();
     expect($movie->getVideos())->toBeArray();
 
     $trailer = $movie->getVideoType('Trailer');
-    expect($trailer)->toBeInstanceOf(TmdbVideo::class);
-    expect($trailer->getType())->toBe('Trailer');
-    expect($trailer)->not()->toBeNull();
-    expect($trailer->getYouTubeUrl())->toBeString();
+    videoIs($trailer, 'Trailer');
+});
+
+it('can get tv show teaser', function () {
+    $movie = apiCallTvShow();
+    expect($movie->getVideos())->toBeArray();
+
+    $teaser = $movie->getVideoTeaser();
+    videoIs($teaser, 'Teaser');
+});
+
+it('can get tv show trailer', function () {
+    $movie = apiCallTvShow();
+    expect($movie->getVideos())->toBeArray();
+
+    $trailer = $movie->getVideoTrailer();
+    videoIs($trailer, 'Trailer');
+});
+
+it('can get tv show promo', function () {
+    $movie = apiCallTvShow();
+    expect($movie->getVideos())->toBeArray();
+
+    $promo = $movie->getVideoPromo();
+    videoIs($promo, 'Trailer');
+});
+
+it('can get tv show trailer type', function () {
+    $movie = apiCallTvShow();
+    expect($movie->getVideos())->toBeArray();
+
+    $trailer = $movie->getVideoType('Trailer');
+    videoIs($trailer, 'Trailer');
 });
